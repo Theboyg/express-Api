1e75ffbd5dd2fa3dc9a226d994a85bea783247de
Gaoussou Diallo <gaoussou1993@gmail.com>
2021-06-08 13:52:48 -0700

    configuring dev dependencies babel, nodemon eslint, prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
+import express from 'express';
+const IndexRouter = express.Router();
+IndexRouter.get('/', function (req, res, next) {
   return res.status(200).json({ message: 'Welcome to Express API template' });
 });
-module.exports = router;
+export default IndexRouter;
