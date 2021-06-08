59c844949a5dd3f00989e5ac9d4ee7f96026874e
Gaoussou Diallo <gaoussou1993@gmail.com>
2021-06-08 14:33:14 -0700

    Settings And Environment Variables In Our .Env File

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
-const IndexRouter = express.Router();
-IndexRouter.get('/', function (req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
-});
-export default IndexRouter;
+import { testEnvironmentVariable } from '../settings';
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res) =>
+  res.status(200).json({ message: testEnvironmentVariable })
+);
+
+export default indexRouter;
